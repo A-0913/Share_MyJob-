6a2575d0a5c9f348e506bc6dd860d6bb8fef56ba
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-03 13:11:51 +0000

    [update]admin db 作成

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,13 +1,6 @@
-class Member < ApplicationRecord
+class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :nickname, presence: true
-  validates :email, presence: true
-
-
 end
