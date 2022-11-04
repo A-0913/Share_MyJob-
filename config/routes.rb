322fdc14828d8e808b2358972d9b4f1dab97d823
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-04 04:08:40 +0000

    [update]管理者ログイン・ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   root to: "public/homes#top"
   get 'about'=> 'public/homes#about'
+  get 'admin'=> 'admin/homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
