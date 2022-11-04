15123cc2addcce7a0bfe14c655d27771ecae55de
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-04 05:35:47 +0000

    [update]画面遷移先設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'homes/top'
-  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
