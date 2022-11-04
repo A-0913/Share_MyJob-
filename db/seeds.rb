0917a4730e430d25a7efba5c242c27e65743a63c
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-04 03:48:43 +0000

    [update]seedファイルにメンバー2人と管理者追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Member.create!(
+  id:1,
+  last_name:'テスト',
+  first_name:'太郎',
+  nickname:'Taro',
+  introduction:'よろしくお願いします！',
+  email:'test1@test.jp',
+  password:'111111',
+  belong:'転職を考え中',
+)
+
+Member.create!(
+  id:2,
+  last_name:'テスト',
+  first_name:'花子',
+  nickname:'hana',
+  introduction:'転職しようか悩んでいます。',
+  email:'test2@test.jp',
+  password:'222222',
+  belong:'転職を考え中',
+)
+
+Admin.create!(
+  email:'admin@admin.jp',
+  password:'password'
+)
