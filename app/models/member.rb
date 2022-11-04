e73f5e3bde30967f5bbba733ba5d5d2b4c8d4d0d
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-04 06:55:21 +0000

    [update]マイページ詳細画面作成

--- app/models/member.rb
+++ app/models/member.rb
@@ -9,6 +9,16 @@ class Member < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true
 
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/No_image.png')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |member|
       #SecureRandom:ランダムパスワードにすることで変更されることを防ぐ。
