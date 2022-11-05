3428d74fe2e89f6b687ca9617c2bad3e50491a75
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-05 00:24:08 +0000

    [update]Model/controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
