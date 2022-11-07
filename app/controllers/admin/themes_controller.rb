16e964f85ef9f852fc462f6395d7a8a6cf34fcc1
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-07 07:30:51 +0000

    [update]テーマ詳細画面・編集画面のみ作成2（controller未成）

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -8,10 +8,26 @@ class Admin::ThemesController < ApplicationController
     # @themes = @job.themes.all
     # @theme = Theme.where(job_id: @Job)
   end
-  
+
   def edit
     # @theme = Theme.where(job_id: @Job)
   end
-  
-  
+
+  def update
+    @theme = Theme.where(job_id: @Job)
+    if @theme.update(theme_update_params)
+       flash[:notice] = "更新が成功しました!"
+       redirect_to admin_theme_path(@theme)
+    else
+       flash[:notice] = "更新が正常に行われませんでした。内容をご確認ください。"
+       render :edit
+    end
+  end
+
+  private
+    def theme_update_params
+      params.require(:theme).permit( :name, :reason, :is_published, :is_checked)
+    end
+
+
 end
