e919ce8828bda0abdd21ef3e97a22932403e1bf8
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-07 13:03:08 +0000

    [update]管理者側テーマ編集機能作成

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -5,19 +5,18 @@ class Admin::ThemesController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
-    # byebug
     @themes = @job.themes.order("updated_at DESC")
   end
 
   def edit
-    # @theme = Theme.where(job_id: @Job)
+    @theme = Theme.find(params[:id])
   end
 
   def update
-    @theme = Theme.where(job_id: @Job)
+    @theme = Theme.find(params[:id])
     if @theme.update(theme_update_params)
        flash[:notice] = "更新が成功しました!"
-       redirect_to admin_theme_path(@theme)
+       redirect_to action: :index
     else
        flash[:notice] = "更新が正常に行われませんでした。内容をご確認ください。"
        render :edit
