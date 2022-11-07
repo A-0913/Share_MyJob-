e89919a43f78f927dfbe8777d130647d33f5dd85
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-07 04:01:30 +0000

    [update]管理者側職業編集機能実装

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -9,6 +9,23 @@ class Admin::JobsController < ApplicationController
   end
 
   def edit
+    @job = Job.find(params[:id])
   end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_update_params)
+       flash[:notice] = "更新が成功しました!"
+       redirect_to admin_job_path(@job)
+    else
+       flash[:notice] = "更新が正常に行われませんでした。内容をご確認ください。"
+       render :edit
+    end
+  end
+
+  private
+    def job_update_params
+      params.require(:job).permit(:genre_id, :name, :reason, :is_published, :is_checked)
+    end
 
 end
