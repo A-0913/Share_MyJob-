a3c20d5e1f9b109c739daba61812c96e281978c2
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-07 07:27:36 +0000

    [update]テーマ詳細画面・編集画面のみ作成（controller未成立）

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -1,10 +1,17 @@
 class Admin::ThemesController < ApplicationController
   def index
-  end
-
-  def edit
+    @Jobs = Job.all
   end
 
   def show
+    @Job = Job.find(params[:id])
+    # @themes = @job.themes.all
+    # @theme = Theme.where(job_id: @Job)
   end
+  
+  def edit
+    # @theme = Theme.where(job_id: @Job)
+  end
+  
+  
 end
