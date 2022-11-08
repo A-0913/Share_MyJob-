754ea632346abd0ba90de0adcd79ebef95cf77d1
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-08 00:02:04 +0000

    [update]model作成及びアソシエーション設定

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,6 +2,7 @@ class Theme < ApplicationRecord
   belongs_to :member
   has_many :theme_in_jobs
   has_many :jobs, through: :theme_in_jobs
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :reason, presence: true
