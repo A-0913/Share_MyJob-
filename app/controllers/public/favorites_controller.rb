5a1a40c6d1ad53ea8ffcffac7fa40f3b65590fbd
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-09 04:13:03 +0000

    [update]いいね機能実装（レイアウト未着手）

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,2 +1,22 @@
 class Public::FavoritesController < ApplicationController
+
+  def create
+    @job = Job.find(params[:job_id])
+    @theme = Theme.find(params[:theme_id])
+    comment = Comment.find(params[:comment_id])
+    favorite = current_member.favorites.new(comment_id: comment.id)
+    favorite.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @job = Job.find(params[:job_id])
+    @theme = Theme.find(params[:theme_id])
+    comment = Comment.find(params[:comment_id])
+    favorite = current_member.favorites.find_by(comment_id: comment.id)
+    favorite.destroy
+    redirect_to request.referer
+  end
+
+
 end
