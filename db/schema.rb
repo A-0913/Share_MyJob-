562bd8b61d2ca1881b2e7c54387f879e6d0df9f3
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-12 06:34:56 +0000

    [update]デプロイ前修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_09_021428) do
+ActiveRecord::Schema.define(version: 2022_11_12_063305) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,12 +53,12 @@ ActiveRecord::Schema.define(version: 2022_11_09_021428) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "comment"
+    t.text "comment", null: false
     t.integer "member_id", null: false
     t.integer "theme_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "job_id"
+    t.integer "job_id", null: false
   end
 
   create_table "favorites", force: :cascade do |t|
