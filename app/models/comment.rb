78f453bad4d85a63b351db4e1e0581cabc3c5d63
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-14 13:26:06 +0000

    [update]modelアソシエーション

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :member
   belongs_to :job
   belongs_to :theme
+
   has_many :favorites, dependent: :destroy
+  has_many :reports, dependent: :destroy
 
   validates :comment, presence: true
 
@@ -11,6 +13,14 @@ class Comment < ApplicationRecord
       false
     else
       favorites.exists?(member_id: member.id)
+    end
+  end
+
+  def reported_by?(member)
+    if member.nil?
+      false
+    else
+      reports.exists?(member_id: member.id)
     end
   end
 
