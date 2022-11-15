2608945fd9e35aa6f6349582cb613a738c1a60e9
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-15 12:06:22 +0000

    [update]Theme_show画面修正

--- app/controllers/public/reports_controller.rb
+++ app/controllers/public/reports_controller.rb
@@ -3,19 +3,22 @@ class Public::ReportsController < ApplicationController
     @job = Job.find(params[:job_id])
     @theme = Theme.find(params[:theme_id])
     @comment = Comment.find(params[:comment_id])
-    #@report = Report.new
+    @report = Report.new
   end
 
   def create
     @comment = Comment.find(params[:comment_id])
     @report = Report.new(report_params)
     @report.member_id = current_member.id
-    @report.comment_id = Comment.find(params[:comment_id])
-    #binding.pry
+    @report.comment = Comment.find(params[:comment_id])
+    # binding.pry
     if @report.save
       flash[:notice] = "通報を受け付けました。ご報告ありがとうございました。"
       redirect_to request.referer
     else
+      @job = Job.find(params[:job_id])
+      @theme = Theme.find(params[:theme_id])
+      @comment = Comment.find(params[:comment_id])
       render "new"
     end
   end
@@ -23,7 +26,7 @@ class Public::ReportsController < ApplicationController
   private
 
   def report_params
-    params.permit(:reason)
+    params.require(:report).permit(:reason)
   end
 
 
