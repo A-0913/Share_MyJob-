86488ddd1f516afd23a9a8f86b1c3251009e25e4
EC2 Default User <ayaka-31014313@outlook.jp>
2022-11-16 12:58:40 +0000

    [update]表示カラム修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -20,7 +20,7 @@ before_action :authenticate_any!
     job = Job.find(params[:job_id])
     @theme = Theme.find(params[:theme_id])
     comment = Comment.find(params[:id])
-    comment.destroy
+    comment.update(is_deleted: true)
     #redirect_to job_theme_path(job,@theme)
   end
 
@@ -29,5 +29,7 @@ before_action :authenticate_any!
   def comment_params
     params.require(:comment).permit(:comment)
   end
+  
+
 
 end
