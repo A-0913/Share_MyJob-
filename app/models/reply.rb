6f1966f817c04ea30c7f8a3e04109489e161391a
EC2 Default User <ayaka-31014313@outlook.jp>
2022-12-10 14:48:28 +0000

    [update]返信コメントに誹謗中傷対策

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -6,8 +6,20 @@ class Reply < ApplicationRecord
 
   validates :reply, presence: true
 
+  validate :slander_check
+
   def reported_by?(member, reply)
     reports.exists?(member_id: member.id, reply_id: reply.id)
+  end
+
+  def slander_check
+    slander_words = ["バカ","きもい","死ね","ブサイク","アホ","殺す","キモい","クズ","消えろ","失せろ","ブス","クソ","ばか"]
+    slander_words.each do |word|
+      if self.reply.include?(word)
+        errors.add(:reply, "内に人を傷つける可能性のある言葉が含まれています。\n内容をご確認の上、再度ご入力をお願いします。")
+        break
+      end
+    end
   end
 
 end
