679ce7867e53a434b53da60b23d0116667faaa06
EC2 Default User <ayaka-31014313@outlook.jp>
2022-12-12 15:07:43 +0000

    [update]テーマ編集画面修正

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -14,12 +14,17 @@ class Reply < ApplicationRecord
 
   def slander_check
     slander_words = ["バカ","きもい","死ね","ブサイク","アホ","殺す","キモい","クズ","消えろ","失せろ","ブス","クソ","ばか"]
-    slander_words.each do |word|
-      if self.reply.include?(word)
-        errors.add(:reply, "内に人を傷つける可能性のある言葉が含まれています。\n内容をご確認の上、再度ご入力をお願いします。")
-        break
-      end
+    slander_word = slander_words.find {|w| self.reply.include?(w) }
+    if slander_word.present?
+      errors.add(:reply, "内に人を傷つける可能性のある言葉(※#{slander_word})が含まれています。\n内容をご確認の上、ご入力をお願いします。")
     end
+
+    # slander_words.each do |word|
+    #   if self.reply.include?(word)
+    #     errors.add(:reply, "内に人を傷つける可能性のある言葉が含まれています。\n内容をご確認の上、再度ご入力をお願いします。")
+    #     break
+    #   end
+    # end
   end
 
 end
