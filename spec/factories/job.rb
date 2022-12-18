11812eff4f59d342f479334e5d63aaf08370dcd7
EC2 Default User <ayaka-31014313@outlook.jp>
2022-12-18 02:12:54 +0000

    [update]一時保存

--- spec/factories/job.rb
+++ spec/factories/job.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :job do
+    association :member
+    association :genre
     member_id { 1 }
     genre_id { 1 }
     name { Faker::Lorem.characters(number:5) }
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -4,31 +4,23 @@ require 'rails_helper'
 
 RSpec.describe Job, "モデルに関するテスト", type: :model do
   describe '実際に保存してみる' do
-    member = Member.new(id:'1')
-    member.save
-    genre = Genre.new(id:'1')
-    member.save
-    job = Job.new(member_id:'', genre_id:'', name:'')
     it "有効な投稿内容の場合は保存されるか" do
-      #expect(FactoryBot.build(:job)).to be_valid
-      job.member_id = 1
-      job.genre_id = 1
-      job.name = 'hoge'
+      job = build(:job)
       expect(job).to be_valid
     end
   end
-  context '空白のバリデーションチェック' do
-    it "genreが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      #job = Job.new(member_id: '1', genre_id: '', name:'hoge')
-      expect(job).to be_invalid
-      expect(job.errors[:genre]).to include("ジャンルを入力してください")
-    end
-    it "nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      #job = Job.new(member_id: '1', genre_id: '1', name:'')
-      expect(job).to be_invalid
-      expect(job.errors[:name]).to include("職業名を入力してください")
-    end
-  end
+  # context '空白のバリデーションチェック' do
+  #   it "genreが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+  #     #job = Job.new(member_id: '1', genre_id: '', name:'hoge')
+  #     expect(job).to be_invalid
+  #     expect(job.errors[:genre]).to include("ジャンルを入力してください")
+  #   end
+  #   it "nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+  #     #job = Job.new(member_id: '1', genre_id: '1', name:'')
+  #     expect(job).to be_invalid
+  #     expect(job.errors[:name]).to include("職業名を入力してください")
+  #   end
+  # end
 end
 
 
