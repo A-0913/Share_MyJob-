4b4595bc038ed22e62f9da3cc200e3c502a32900
EC2 Default User <ayaka-31014313@outlook.jp>
2022-12-21 03:57:06 +0000

    [update]member&genreモデルテストコード修正

--- spec/models/devise_members_spec.rb
+++ spec/models/devise_members_spec.rb
@@ -1,49 +1,52 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Member, "モデルに関するテスト", type: :model do
-  describe '実際に保存してみる' do
+  let!(:member) { build(:member) }
+
+  describe "実際に保存してみる" do
     it "有効な入力内容の場合は保存されるか" do
-      member = build(:member)
       expect(member).to be_valid
     end
   end
-  context '空白のバリデーションチェック' do
+
+  context "空白のバリデーションチェック" do
     it "last_nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      member = Member.new(last_name: '', first_name:'hoge', nickname:'hoge', email:'hoge@example.com', password:'hogehoge', password_confirmation:'hogehoge', belong:'転職を検討中')
+      member.last_name = ""
       expect(member).to be_invalid
       expect(member.errors[:last_name]).to include("を入力してください")
     end
     it "first_nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      member = Member.new(last_name: 'hoge', first_name:'', nickname:'hoge', email:'hoge@example.com', password:'hogehoge', password_confirmation:'hogehoge', belong:'転職を検討中')
+      member.first_name = ""
       expect(member).to be_invalid
       expect(member.errors[:first_name]).to include("を入力してください")
     end
     it "nicknameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      member = Member.new(last_name: 'hoge', first_name:'hoge', nickname:'', email:'hoge@example.com', password:'hogehoge', password_confirmation:'hogehoge', belong:'転職を検討中')
+      member.nickname = ""
       expect(member).to be_invalid
       expect(member.errors[:nickname]).to include("を入力してください")
     end
     it "emailが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      member = Member.new(last_name: 'hoge', first_name:'hoge', nickname:'hoge', email:'', password:'hogehoge', password_confirmation:'hogehoge', belong:'転職を検討中')
+      member.email = ""
       expect(member).to be_invalid
       expect(member.errors[:email]).to include("を入力してください")
     end
     it "passwordが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      member = Member.new(last_name: 'hoge', first_name:'hoge', nickname:'hoge', email:'hoge@example.com', password:'', password_confirmation:'', belong:'転職を検討中')
+      member.password = ""
       expect(member).to be_invalid
       expect(member.errors[:password]).to include("を入力してください")
     end
     it "belongが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      member = Member.new(last_name: 'hoge', first_name:'hoge', nickname:'hoge', email:'hoge@example.com', password:'hogehoge', password_confirmation:'hogehoge', belong:'')
+      member.belong = ""
       expect(member).to be_invalid
       expect(member.errors[:belong]).to include("を入力してください")
     end
   end
-  context '入力用と確認用のパスワードの整合性バリデーションチェック' do
+  context "入力用と確認用のパスワードの整合性バリデーションチェック" do
     it "入力用と確認用のパスワードが一致していない場合にエラーメッセージが返ってきているか" do
-      member = Member.new(last_name: 'hoge', first_name:'hoge', nickname:'hoge', email:'hoge@example.com', password:'hogehoge', password_confirmation:'hogehogehoge', belong:'転職を検討中')
+      member.password = "hogehoge"
+      member.password_confirmation = "hogehogehoge"
       expect(member).to be_invalid
       expect(member.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Genre, "モデルに関するテスト", type: :model do
-  describe '実際に保存してみる' do
+
+  let!(:genre) { build(:genre) }
+
+  describe "実際に保存してみる" do
     it "有効な投稿内容の場合は保存されるか" do
-      genre = build(:genre)
       expect(genre).to be_valid
     end
   end
-  context '空白のバリデーションチェック' do
+
+  context "空白のバリデーションチェック" do
+    before do
+      genre.name = ""
+    end
     it "genreが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-      genre = Genre.new(name:'')
       expect(genre).to be_invalid
       expect(genre.errors[:name]).to include("を入力してください")
     end
   end
+
 end
