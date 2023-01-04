43a169b52409dbccf7e25d4a1cb67e86ee39fc2c
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-04 02:23:07 +0000

    [update]コメント及び返信モデルのslander_words配列コードの修正

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -13,7 +13,8 @@ class Reply < ApplicationRecord
   end
 
   def slander_check
-    slander_words = ["バカ","きもい","死ね","ブサイク","アホ","殺す","キモい","クズ","消えろ","失せろ","ブス","クソ","ばか"]
+    slander_words = %w(バカ きもい 死ね ブサイク アホ 殺す キモい クズ 消えろ 失せろ ブス クソ ばか)
+
     slander_word = slander_words.find {|w| self.reply.include?(w) }
     if slander_word.present?
       errors.add(:reply, "内に人を傷つける可能性のある言葉(※#{slander_word})が含まれています。\n内容をご確認の上、ご入力をお願いします。")
