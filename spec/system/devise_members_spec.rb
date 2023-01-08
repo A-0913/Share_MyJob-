9352daf21dfa25b76555062cca9d66fcea7facf5
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-08 03:36:03 +0000

    [update]system/devise_members_spec.rb修正

--- spec/system/devise_members_spec.rb
+++ spec/system/devise_members_spec.rb
@@ -40,7 +40,7 @@ describe '会員登録のテスト' do
         click_button '新規登録'
         expect(page).to have_content 'アカウント登録が完了しました。'
       end
-      it '登録に失敗しエラーメッセージが表示されるか' do
+      it '登録に失敗した場合エラーメッセージが表示されるか(必須項目空欄の場合)' do
         fill_in 'member[last_name]', with: ''
         fill_in 'member[first_name]', with: ''
         fill_in 'member[nickname]', with: ''
@@ -49,9 +49,9 @@ describe '会員登録のテスト' do
         fill_in 'member[password_confirmation]', with: ''
         fill_in 'member[introduction]', with: ''
 
-       click_button '新規登録'
-       expect(page).to have_content '保存されませんでした'
-       expect(current_path).to eq('/members')
+        click_button '新規登録'
+        expect(page).to have_content '保存されませんでした'
+        expect(current_path).to eq('/members')
       end
       it '登録後のリダイレクト先は正しいか' do
         fill_in 'member[last_name]', with: Faker::Lorem.characters(number:5)
@@ -90,7 +90,7 @@ describe '会員登録のテスト' do
           expect(page).to have_link '退会する', href: "/members/confirm"
         end
         it 'マイページへ戻るボタンが表示される' do
-          expect(page).to have_link 'マイページへ戻る', href: "/members/1"
+          expect(page).to have_link 'マイページへ戻る', href: "/members/#{member.id}"
         end
         it '情報編集に必要な入力欄が同一のページに表示されているか' do
           expect(page).to have_field 'member[last_name]'
@@ -103,7 +103,7 @@ describe '会員登録のテスト' do
         end
       end
       context '編集処理に関するテスト' do
-        it '編集に成功しサクセスメッセージが表示されるか（パスワードの変更を含まない）' do
+        it 'パスワードの変更を含まない編集に成功した場合、サクセスメッセージが表示されるか' do
           fill_in 'member[last_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[first_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[nickname]', with: Faker::Lorem.characters(number:5)
@@ -114,7 +114,7 @@ describe '会員登録のテスト' do
           click_button '更新する'
           expect(page).to have_content '情報を更新しました'
         end
-          it '編集に成功しサクセスメッセージが表示されるか（パスワードの変更を含む）' do
+          it 'パスワードの変更を含む編集に成功した場合、再ログインを促すメッセージが表示されるか' do
           fill_in 'member[last_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[first_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[nickname]', with: Faker::Lorem.characters(number:5)
@@ -126,7 +126,7 @@ describe '会員登録のテスト' do
           click_button '更新する'
           expect(page).to have_content 'ログインもしくはアカウント登録してください'
         end
-        it '編集に失敗しエラーメッセージが表示されるか' do
+        it '編集に失敗した場合、エラーメッセージが表示されるか' do
           fill_in 'member[last_name]', with: ''
           fill_in 'member[first_name]', with: ''
           fill_in 'member[nickname]', with: ''
@@ -136,9 +136,9 @@ describe '会員登録のテスト' do
 
          click_button '更新する'
          expect(page).to have_content 'のエラーが発生しました'
-         expect(current_path).to eq('/members/1')
+         expect(current_path).to eq("/members/#{member.id}")
         end
-        it '編集（パスワードの変更を含まない）後のリダイレクト先は正しいか' do
+        it 'パスワードの変更を含まない編集後のリダイレクト先は正しいか' do
           fill_in 'member[last_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[first_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[nickname]', with: Faker::Lorem.characters(number:5)
@@ -149,7 +149,7 @@ describe '会員登録のテスト' do
           click_button '更新する'
           expect(page).to have_current_path member_path(member)
         end
-          it '編集（パスワードの変更を含む）後のリダイレクト先は正しいか' do
+          it 'パスワードの変更を含む編集後のリダイレクト先は正しいか' do
           fill_in 'member[last_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[first_name]', with: Faker::Lorem.characters(number:5)
           fill_in 'member[nickname]', with: Faker::Lorem.characters(number:5)
@@ -183,7 +183,7 @@ describe '会員登録のテスト' do
           expect(page).to have_link '退会する', href: "/members/withdraw"
         end
         it 'マイページに戻るボタンが表示される' do
-          expect(page).to have_link 'マイページに戻る', href: "/members/1"
+          expect(page).to have_link 'マイページに戻る', href: "/members/#{member.id}"
         end
       end
       context '退会処理に関するテスト' do
